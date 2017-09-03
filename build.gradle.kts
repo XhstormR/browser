c031719d169c7fb2a4fb111f533b79c8ae2028d0
XhstormR <674080894@qq.com>
2017-09-03 20:55:17 +0800

    Signed-off-by: XhstormR <X>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,4 @@
 import org.gradle.api.tasks.bundling.Jar
-import org.gradle.api.tasks.wrapper.Wrapper
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.springframework.boot.gradle.run.BootRunTask
 
@@ -8,15 +7,12 @@ version = "1.0-SNAPSHOT"
 plugins {
     idea
     application
-}
-
-apply {
-    plugin("kotlin")
-    plugin("org.springframework.boot")
+    kotlin("jvm")
+    id("org.springframework.boot") version ("1.5.7.RELEASE")
 }
 
 task<Wrapper>("wrapper") {
-    gradleVersion = "4.1"
+    gradleVersion = "4.2"
     distributionType = Wrapper.DistributionType.ALL
 }
 
@@ -49,28 +45,22 @@ java {
 }
 
 buildscript {
-    extra["kotlin_version"] = "+"
-    extra["spring_version"] = "+"
-
-    extra["thymeleaf.version"] = "+"
-    extra["thymeleaf-layout-dialect.version"] = "+"
-
     repositories {
-        maven { setUrl("http://maven.aliyun.com/nexus/content/groups/public/") }
+        maven("http://maven.aliyun.com/nexus/content/groups/public/")
     }
 
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${extra["kotlin_version"]}")
-        classpath("org.springframework.boot:spring-boot-gradle-plugin:${extra["spring_version"]}")
     }
 }
 
 repositories {
-    maven { setUrl("http://maven.aliyun.com/nexus/content/groups/public/") }
+    maven("http://maven.aliyun.com/nexus/content/groups/public/")
 }
 
 dependencies {
     compile("org.jetbrains.kotlin:kotlin-stdlib-jre8")
     compile("org.springframework.boot:spring-boot-starter-thymeleaf")
+    compile("org.springframework.boot:spring-boot-starter-actuator")
     compile("org.springframework.boot:spring-boot-devtools")
 }
